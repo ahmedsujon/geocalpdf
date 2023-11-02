662f8fdef15bd5afd8d54d76f1932cfca0a627ed
ahmedsujon <sujonahmed424@gmail.com>
2023-11-02 15:16:42 +0600

    office address update

--- resources/views/livewire/templates/inspection-concrete/create-inspection-concrete-component.blade.php
+++ resources/views/livewire/templates/inspection-concrete/create-inspection-concrete-component.blade.php
@@ -236,13 +236,11 @@
                             </div>
                             <div class="col-md-3"></div>
                         </div>
-
                         <div class="container">
                             <div class="row">
-                                <div class="col-lg-6 select-border">
-                                    <h6 style="font-weight: 700;">REFERENCE DATA</h6>
-                                    <div class="input-group">
-                                        <span class="input-group-text width20">Office Address:</span>
+                                <div class="col-md-12">
+                                    <div class="input-group" style="border: 1px solid #111;">
+                                        <span class="input-group-text" style="border: none">Office Address:</span>
                                         <select class="form-select" wire:model="office_address">
                                             <option value="">Select office addres</option>
                                             <option value="7290 South Fraser St.Centennial, CO 80112">7290 South Fraser
@@ -257,6 +255,14 @@
                                     @error('office_address')
                                         <span class="text-danger" style="font-size: 12px;">{{ $message }}</span>
                                     @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-6 select-border">
+                                    <h6 style="font-weight: 700;">REFERENCE DATA</h6>
                                     <div class="input-group">
                                         <span class="input-group-text width20">Type of Structure:</span>
                                         <input type="text" class="form-control form-color"
