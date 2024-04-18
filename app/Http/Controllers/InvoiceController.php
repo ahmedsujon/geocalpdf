f37518a42fcad2a49d3bcb2bd967bf78ab8297e5
ahmedsujon <sujonahmed424@gmail.com>
2024-04-18 11:44:14 +0600

    download

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -56,7 +56,7 @@ class InvoiceController extends Controller
         $data->client_company_name = client($data->client_id)->company_name;
 
         $pdf = Pdf::loadView('pdf.compressive-strength', compact('data'));
-        return $pdf->stream('compressive-strength-local-agency.pdf');
+        return $pdf->download('compressive-strength-local-agency.pdf');
     }
     public function commercialPDF($id)
     {
